eb424e47013dd4fb46985fed6c140bd4801e4621
Jeff Albrecht <geospatialjeff@gmail.com>
2020-12-11 20:18:32 -0600

    switch to pytest_pgsql

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,42 +1,34 @@
 """``pytest`` configuration."""
 
 import os
-import tempfile
 
-import psycopg2
 import pytest
-from pytest_postgresql import factories
+import pytest_pgsql
 
 from starlette.testclient import TestClient
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), "fixtures")
 
 
-# Create a temporary postgresql instance for the test session, running on :5432
-postgresql_port = 5432
-socket_dir = tempfile.TemporaryDirectory()
-postgresql_my_proc = factories.postgresql_proc(
-    unixsocketdir=socket_dir.name, port=postgresql_port,
+test_db = pytest_pgsql.TransactedPostgreSQLTestDB.create_fixture(
+    "test_db", scope="session", use_restore_state=False
 )
 
 
 @pytest.fixture(scope="session")
-def database_url(postgresql_my_proc):
+def database_url(test_db):
     """
     Session scoped fixture to launch a postgresql database in a separate process.  We use psycopg2 to ingest test data
     because pytest-asyncio event loop is a function scoped fixture and cannot be called within the current scope.  Yields
     a database url which we pass to our application through a monkeypatched environment variable.
     """
-    db_url = f"postgresql://{postgresql_my_proc.user}:{postgresql_my_proc.password}@{postgresql_my_proc.host}:{postgresql_port}"
-    conn = psycopg2.connect(dsn=db_url)
-    cursor = conn.cursor()
-    cursor.execute("CREATE EXTENSION postgis")
-    cursor.execute(open(os.path.join(DATA_DIR, "landsat_wrs.sql")).read())
-    yield db_url
-    conn.close()
+    assert test_db.install_extension("postgis")
+    test_db.run_sql_file(os.path.join(DATA_DIR, "landsat_wrs.sql"))
+    assert test_db.has_table("landsat_wrs")
+    return test_db.connection.engine.url
 
 
-@pytest.fixture
+@pytest.fixture(autouse=True)
 def app(database_url, monkeypatch):
     """Create app with connection to the pytest database."""
     monkeypatch.setenv("DATABASE_URL", database_url)
